8b4c83397a34c508ec02e13a260c883bdf34e557
dk1531 <dhavalchaudhary2014@gmail.com>
2021-03-22 12:27:16 +0530

    domain upgrade

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,3 @@
 cd app/
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug \
-          -p $PORT
+rasa run --model models --enable-api --cors "*" --debug \-p $PORT
